0b9ed47c81e4ad6937b124e281f6acd6f0b1bd6f
christ pangou <christpangou55@gmail.com>
2026-05-19 00:02:33 +0200

    revert: remove client dashboard links and routes

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,6 @@ import AdminLogements from './pages/admin/AdminLogements';
 import AdminLogementForm from './pages/admin/AdminLogementForm';
 import MyLogements from './pages/vendeur/MyLogements';
 import VendeurLogementForm from './pages/vendeur/LogementForm';
-import ClientDashboard from './pages/client/Dashboard';
 import PaymentSuccess from './pages/PaymentSuccess';
 import PaymentCancel from './pages/PaymentCancel';
 import Residences from './pages/Residences';
@@ -49,9 +48,6 @@ const App = () => {
                     <Route path="vendeur/logements/create" element={<VendeurLogementForm />} />
                     <Route path="vendeur/logements/:id/edit" element={<VendeurLogementForm />} />
 
-                    {/* Dashboard Client */}
-                    <Route path="dashboard" element={<ClientDashboard />} />
-                    
                     {/* Routes de confirmation de paiement (Stripe) */}
                     <Route path="payment/success" element={<PaymentSuccess />} />
                     <Route path="payment/cancel" element={<PaymentCancel />} />
